e73aea75c30de539e8dfcc5bf25667a81ccaf579
bemadeveloper <akbermet1990@icloud.com>
2024-02-14 08:48:19 +0600

    Adding UI elements

--- TomatoApp/Sources/ViewController.swift
+++ TomatoApp/Sources/ViewController.swift
@@ -1,19 +1,87 @@
-//
-//  ViewController.swift
-//  TomatoApp
-//
-//  Created by Bema on 7/2/24.
-//
-
+import SnapKit
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - UI
+    var isWorkTime = true
+    var isStarted = false
+    var timer: Timer?
+    
+    let shapeView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "circle.tomato")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let timerLabel: UILabel = {
+        let label = UILabel()
+        label.text = "25:00"
+        label.font = UIFont.systemFont(ofSize: 70, weight: .light)
+        label.textColor = .red
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    // MARK: - Timer
+
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemCyan
+        setupView()
+        setupHierarchy()
+        setupLayout()
     }
-
-
+    
+    
+    // MARK: - Setups
+    
+    private func setupView() {
+        view.backgroundColor = .white
+    }
+    
+    private func setupHierarchy() {
+        
+    }
+    
+    private func setupLayout() {
+        view.addSubview(shapeView)
+        NSLayoutConstraint.activate([
+            shapeView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            shapeView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            shapeView.heightAnchor.constraint(equalToConstant: 300),
+            shapeView.widthAnchor.constraint(equalToConstant: 300)
+        ])
+        shapeView.addSubview(timerLabel)
+        NSLayoutConstraint.activate([
+            timerLabel.centerXAnchor.constraint(equalTo: shapeView.centerXAnchor),
+            timerLabel.centerYAnchor.constraint(equalTo: shapeView.centerYAnchor)
+        ])
+    }
+    
+    // MARK: - Actions
+    
+    // MARK: - Tables
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
